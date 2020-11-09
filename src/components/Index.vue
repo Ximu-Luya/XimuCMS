a65082a8157bffbe0d7fa94abe8003a984a454cd
Ximu_Luya <794876939@qq.com>
2020-11-10 01:58:08 +0800

    * 头部导航完成 * 首页封面完成 * 介绍部分架构完成

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,15 +1,104 @@
 <template>
     <div class="index">
+        <!-- 封面 -->
+        <div class="banner" :style="{backgroundImage: 'url(' + Img + ')'}">
+            <div class="slogan">
+                <p>西木工作室</p>
+                <p>青春永无止境</p>
+            </div>
 
+            <i class="el-icon-d-arrow-right" id="slip" />
+        </div>
+
+        <!-- 团队介绍 -->
+        <div class="introductions" id="summary">
+            <p>团队介绍</p>
+        </div>
+
+        <!-- 研究方向 -->
+        <div class="introductions" id="research">
+            <p>研究方向</p>
+
+        </div>
+
+        <!-- 项目成果 -->
+        <div class="introductions" id="project">
+            <p>项目成果</p>
+        </div>
+
+        <!-- 重大事件 -->
+        <div class="introductions" id="events">
+            <p>重大事件</p>
+        </div>
+
+        <!-- 团队成员 -->
+        <div class="introductions" id="team">
+            <p>团队成员</p>
+        </div>
     </div>
 </template>
 
 <script>
+import Img from '@/assets/img/banner1.png'
+
 export default {
     name: 'index',
+    data(){
+        return{
+            Img
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.index{
+    >.banner{
+        width: 100vw; height: 100vh;
+        position: relative;
+        background-color: rgba(0,0,0,.5);
+        background-size: cover;
+        display: flex;
+        justify-content: center;
+        // filter: blur(3px);
+        // z-index: -1;
 
+        .slogan{
+            top: 35%;
+            position: absolute;
+            // transform: translate(-50%, -50%);
+            font-size: 34px;
+            color: #fff;
+            text-align: center;
+            line-height: 60px;
+            // z-index: 10;
+
+            p:first-child{
+                font-size: 3rem;
+                color: #ffffff;
+                font-weight: 600;
+            }
+            p:last-child{
+                font-size: 2rem;
+                color: #ffffff9c;
+            }
+        }
+
+        #slip{
+            position: absolute;
+            top: 85%;
+            color: #fff;
+            font-size: 3rem;
+            transform: rotate(90deg);
+        }
+    }
+
+    .introductions{
+        height: 300px;
+        padding: 0 10%;
+        p{
+            font-size: 3rem;
+        }
+    }
+}
 </style>
--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,15 +1,65 @@
 <template>
     <div id="header">
-        
+        <router-link id="logo" to="/index">
+            <img width="50px" style="color: #fff; margin: 0 10px;" :src="Logo" />
+            工作室
+        </router-link>
+
+        <div id="topNav">
+            <router-link to="">首页</router-link>
+            <router-link to="">项目成果</router-link>
+            <router-link to="">博客</router-link>
+            <router-link to="">联系我们</router-link>
+        </div>
     </div>
 </template>
 
 <script>
+import Logo from "@/assets/img/logo.svg";
+
 export default {
-    name: 'Header',
-}
+    name: "Header",
+    data() {
+        return {
+            Logo
+        };
+    },
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+#header{
+    width: 100%;
+    padding: 20px 17%;
+    display: flex;
+    justify-content: space-between;
+    position: fixed;
+    // left: 50%;
+    // transform: translate(-50%, 0);
+    transition: 0.7s all;
+    z-index: 100;
 
+    #logo{
+        display: flex;
+        align-items: center;
+    }
+
+    #topNav{
+        display: flex;
+        align-items: center;
+        width: 80%;
+        justify-content: flex-end;
+    }
+
+    a{
+        color: #fff;
+        font-weight: 500;
+        font-size: 1.5rem;
+        padding: 0 30px;
+    }
+}
+.notTop{
+    box-shadow: 0px 0px 2px #333333;
+    background-color: #535353;
+}
 </style>
